19f1f56f81e9ba9b7d59fec0b32ded71f071cd3c
Rashmika Perera <rashmikagayanperera@gmail.com>
2022-04-18 02:03:07 +0800

    init payslip_api

--- payslip_api/test/controllers/payslips_controller_test.rb
+++ payslip_api/test/controllers/payslips_controller_test.rb
@@ -19,33 +19,11 @@ class PayslipsControllerTest < ActionDispatch::IntegrationTest
     assert_response :bad_request
   end  
 
-  test "should get salary computations" do
-    get '/monthly_payslip', as: :json
-    body = JSON.parse(response.body)
-
-    assert_response :ok
-    assert_equal 2, body["salary_computations"].length
-
-    emp1_sal_computation = body["salary_computations"][1]
-    assert_equal "2022-04-17T04:00:00.000Z", emp1_sal_computation["time_stamp"]
-    assert_equal "Ren", emp1_sal_computation["employee_name"]
-    assert_equal "$60000.00", emp1_sal_computation["annual_salary"]
-    assert_equal "$500.00", emp1_sal_computation["monthly_income_tax"]
-
-    emp2_sal_computation = body["salary_computations"][0]
-    assert_equal "2022-04-17T05:00:00.000Z", emp2_sal_computation["time_stamp"]
-    assert_equal "Rash", emp2_sal_computation["employee_name"]
-    assert_equal "$80150.00", emp2_sal_computation["annual_salary"]
-    assert_equal "$837.08", emp2_sal_computation["monthly_income_tax"]
-  end
-
   test "should generate monthly payslip" do
-    assert_difference('Payslip.count') do
-      post '/monthly_payslip', params: {
-        employee_name: "Ren",
-        annual_salary: 60000
-      }, as: :json
-    end
+    post '/monthly_payslip', params: {
+      employee_name: "Ren",
+      annual_salary: 60000
+    }, as: :json
     body = JSON.parse(response.body)
 
     assert_response :ok
